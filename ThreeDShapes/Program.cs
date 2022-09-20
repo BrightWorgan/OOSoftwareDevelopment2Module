99af2ec62987a87a105c6b7d72ad6891c48b6999
Sarah <sarah@cleancodecompany.com>
2022-09-20 22:17:22 +0100

    first class of OOSD2

---
+++ ThreeDShapes/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public abstract class ThreeDShapes{
+    {
+        private String _type;
+
+        public 
+        string String Type
+        {
+            get
+            {
+                return _type;
+            }
+        }
+    }_
+
+}
