d6b149c1410d5cb32f72629feda9f15850416976
Sarah <sarah@cleancodecompany.com>
2022-11-15 21:26:04 +0000

    class notes

---
+++ recursionClassNotes/recursionClassNotes/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main()
+    {
+       Console.WriteLine( Pow(2, 4));
+    }
+
+    static double Pow(double num, int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+
+        if (n == 1)
+        {
+            return num;
+        }
+
+        return num * Pow(num, n - 1);
+    }
+}
